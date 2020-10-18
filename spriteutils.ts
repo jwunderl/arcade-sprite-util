9c9c1da66f41b02ca53d00aed12fcac0a036b1f7
Joey Wunderlich <jwunderl@users.noreply.github.com>
2020-10-17 18:22:19 -0700

    add block for angle between two sprites

--- spriteutils.ts
+++ spriteutils.ts
@@ -3,6 +3,7 @@
  */
 //% weight=99 color="#4B7BEC" icon="\uf2bd"
 //% block="Sprite Utils"
+//% groups='["Sprite", "General"]'
 namespace spriteutils {
 
     /**
@@ -13,6 +14,7 @@ namespace spriteutils {
     //% blockId=spriteutilextisdestroyed
     //% sprite.shadow=variables_get
     //% sprite.defl=mySprite
+    //% group=Sprite
     export function isDestroyed(sprite: Sprite): boolean {
         return !sprite || !!(sprite.flags & sprites.Flag.Destroyed);
     }
@@ -27,8 +29,28 @@ namespace spriteutils {
     //% a.defl=mySprite
     //% b.shadow=variables_get
     //% b.defl=myEnemy
+    //% group=Sprite
     export function distanceBetween(a: Sprite, b: Sprite): number {
         if (!a || !b) return 0;
         return Math.sqrt(((a.x - b.x) ** 2) + ((a.y - b.y) ** 2));
+    }
+
+    /**
+     * Returns the angle between the center of two sprites in radians.
+     * If either sprite is undefined returns 0.
+     */
+    //% block="angle from $a and $b"
+    //% blockId=spriteutilextanglebw
+    //% a.shadow=variables_get
+    //% a.defl=mySprite
+    //% b.shadow=variables_get
+    //% b.defl=myEnemy
+    //% group=Sprite
+    export function angleFrom(a: Sprite, b: Sprite): number {
+        if (!a || !b) return 0;
+        return Math.atan2(
+            b.y - a.y,
+            b.x - a.x
+        );
     }
 }
