40b83cb775233c34fa0a7cadb7266315b4d47ffa
Joey Wunderlich <jwunderl@users.noreply.github.com>
2020-10-17 18:15:56 -0700

    add block for distance between two sprites

--- spriteutils.ts
+++ spriteutils.ts
@@ -13,7 +13,22 @@ namespace spriteutils {
     //% blockId=spriteutilextisdestroyed
     //% sprite.shadow=variables_get
     //% sprite.defl=mySprite
-    export function isDestroyed(sprite: Sprite) {
-        return !sprite || (sprite.flags & sprites.Flag.Destroyed);
+    export function isDestroyed(sprite: Sprite): boolean {
+        return !sprite || !!(sprite.flags & sprites.Flag.Destroyed);
+    }
+
+    /**
+     * Returns the distance between the center of two sprites in pixels.
+     * If either sprite is undefined returns 0.
+     */
+    //% block="distance between $a and $b"
+    //% blockId=spriteutilextdistbw
+    //% a.shadow=variables_get
+    //% a.defl=mySprite
+    //% b.shadow=variables_get
+    //% b.defl=myEnemy
+    export function distanceBetween(a: Sprite, b: Sprite): number {
+        if (!a || !b) return 0;
+        return Math.sqrt(((a.x - b.x) ** 2) + ((a.y - b.y) ** 2));
     }
 }
